6f8e154c2bceb0ee6d1497fd3a88db5dfbb84bfe
Alan Boles <bolesa@my.lowercolumbia.edu>
2018-07-02 18:21:15 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma (response)
-  if response != response.uppercase
+  if response != response.upcase
     puts "HUH?! SPEAK UP, SONNY!"
   end
 end
