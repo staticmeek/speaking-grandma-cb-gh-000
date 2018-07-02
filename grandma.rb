6eb51876ff93b61c23c26fd7f2da7c724f545ad8
Alan Boles <bolesa@my.lowercolumbia.edu>
2018-07-02 18:20:59 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -2,9 +2,5 @@
 def speak_to_grandma (response)
   if response != response.uppercase
     puts "HUH?! SPEAK UP, SONNY!"
-  elsif response == response.uppercase
-    puts "I LOVE YOU TOO PUMPKIN!"
-  else
   end
-
 end
