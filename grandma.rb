efc084a1871647d2fce25948598a69112fe2ab56
Alan Boles <bolesa@my.lowercolumbia.edu>
2018-07-02 18:17:41 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma (response)
-if response == response.upcase && response == "I LOVE YOU GRANDMA!"
-  puts "I LOVE YOU TOO PUMPKIN!"
-end
+  if response == response.upcase && response == "I LOVE YOU GRANDMA!"
+    puts "I LOVE YOU TOO PUMPKIN!"
+  end
 end
